0c8fd4f6cfef8b3189b56530f8b8388cad55eb4a
toast38coza <info@38.co.za>
2017-06-14 19:50:28 +0200

    Search frontend

---
+++ ui/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <ais-index
+      appId="WTS23NLS2K"
+      apiKey="a49fed1a512e605d8356722bfbb84ebe"
+      indexName="ICD10Code"
+    >
+      <ais-search-box />
+      <ais-results>
+        <template scope="{ result }">
+          <h2>
+            {{result.parent}} > <ais-highlight :result="result" attributeName="code" >
+            </ais-highlight>:
+            <ais-highlight :result="result" attributeName="title" >
+            </ais-highlight>
+          </h2>
+        </template>
+      </ais-results>
+    </ais-index>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
