592986a16c94daae5bbba57d8c66663fb1ecb52d
toast38coza <info@38.co.za>
2017-11-29 23:02:41 +0200

    importing process codes

--- api/api.py
+++ api/api.py
@@ -1,6 +1,11 @@
 from rest_framework import routers, serializers, viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import Code
+from .models import Code, ProcessCode
+
+class ProcessCodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProcessCode
+        fields = '__all__'
 
 class CodeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,6 +22,19 @@ class CodeViewSet(viewsets.ModelViewSet):
     ordering_fields = ('name',)
     ordering = ('name',)
 
+
+class ProcessCodeViewSet(viewsets.ModelViewSet):
+    queryset = ProcessCode.objects.all()
+    serializer_class = ProcessCodeSerializer
+
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+
+    search_fields = ('name', 'description')
+    filter_fields = ('name',)
+    ordering_fields = ('name',)
+    ordering = ('name',)
+
 router = routers.DefaultRouter()
 router.register(r'codes', CodeViewSet)
+router.register(r'processcodes', ProcessCodeViewSet)
 
--- api/management/commands/import_process_codes.py
+++ api/management/commands/import_process_codes.py
@@ -7,14 +7,30 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        with open('data/process/dh_rate_dieticians.csv') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                code = ProcessCode()
-                code.practice_type_code = row[0]
-                code.name = row[1]
-                code.description = row[2]
-                code.discovery_rate = row[3]
-                code.save()
+        [p.delete() for p in ProcessCode.objects.all()]
+        files = [
+            'dh_rate_bio.csv',
+            'dh_rate_dieticians.csv',
+            'dh_rate_psychology.csv',
+            'dh_rate_chiropractor2.csv',
+            'dh_rate_dieticians2.csv',
+            'dh_rate_speech_therapy_and_audiology.csv',
+            #'dh_rate_consult_code.csv',
+            'dh_rate_occupational_therapy.csv',
+            #'dh_rate_dental_guide.csv',
+            'dh_rate_podiatry.csv',
+        ]
+        for file in files:
+            with open('data/process/dh_rate_dieticians.csv') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    code = ProcessCode()
+                    code.practice_type_code = row[0]
+                    code.name = row[1]
+                    code.description = row[2]
+                    if row[3] not in ['Blank']:
+                        code.discovery_rate = row[3]
+                    code.save()
 
-            print ('Imported: {} codes'.format(ProcessCode.objects.count()))
+                print ('Imported: {} codes'.format(ProcessCode.objects.count()))
+
